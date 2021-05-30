caecdd611a89778524e8556627afc5fd8097b6cd
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-05-30 03:40:39 +0300

    city added

---
+++ hrmsProject/src/main/java/Hrms/hrmsProject/entities/concretes/City.java
@@ -0,0 +1,39 @@
+package Hrms.hrmsProject.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cities")
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","jobadvertisements"})
+public class City {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    @Column(name = "city_name")
+    private String cityName;
+    
+    
+    @OneToMany(mappedBy = "city")
+    private List<JobAdvertisement> jobadvertisements;
+}
